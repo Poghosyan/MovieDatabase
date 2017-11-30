61f2e96183ba2fe7143f0fe4bc968ce77099ada3
mpoghosyan <mher.poghosyan@loni.usc.edu>
2017-11-30 11:54:50 -0800

    Wrote functionality for main and edited adRating as well as updateRating to handle movies which are not in the database, even though instructions specified that there would be nor requests for movies not in the database.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,39 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 
+public class Main {
+    /* You create a new instance of a movieDatabase.
+    Add all the movies in the file movies.txt.
+    Go through the ratings of the movies in the file ratings.txt and add the ratings for the movies.
+    Now call the methods that you created and print out the name of the best actor and the name of the highest rated movie.
+     */
+    public static void main(String[] args) {
+        MovieDatabase movieDB = new MovieDatabase();
+        String line;
+        try(BufferedReader reader = new BufferedReader(new FileReader("movies.txt"))) {
+            while((line = reader.readLine()) != null) {
+                String[] inputs = line.split(", ");
+                for(String movie: Arrays.copyOfRange(inputs, 1, inputs.length)) {
+                    movieDB.addMovie(movie, Arrays.copyOfRange(inputs, 0, 1));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try(BufferedReader reader = new BufferedReader(new FileReader("ratings.txt"))) {
+            reader.readLine();
+            while((line = reader.readLine()) != null) {
+                String[] inputs = line.split("\t");
+                movieDB.addRating(inputs[0], Double.parseDouble(inputs[1]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Best actor goes to " + movieDB.getBestActor() + ".");
+        System.out.println("Best motion picture goes to " + movieDB.getBestMovie() + ".");
     }
 }
--- src/main/java/MovieDatabase.java
+++ src/main/java/MovieDatabase.java
@@ -17,7 +17,8 @@ public class MovieDatabase {
      * that movie. If the movie is already in the database, your code ignores this request (this specification is
      * an oversimplification). If the movie is a new movie, a movie object is created and added to the movieList.
      * If any of the actors happen to be new, they will be added to the actorList.
-     * @param name Name of movie which will be added
+     *
+     * @param name   Name of movie which will be added
      * @param actors Array of names of actors which are in the movie cast
      */
     void addMovie(String name, String[] actors) {
@@ -26,18 +27,18 @@ public class MovieDatabase {
         ArrayList<Movie> flixCastIn = new ArrayList<Movie>();
         Movie movie = new Movie(name);
 
-        if(!movieList.contains(movie)) {
+        if (!movieList.contains(movie)) {
             movieCast = addToActorList(movie, actors);
             movie.setActors(movieCast);
             movieList.add(movie);
         }
 
-        for (Actor actorToAdd: movieCast) {
-            if(actorList.contains(actorToAdd)) {
+        for (Actor actorToAdd : movieCast) {
+            if (actorList.contains(actorToAdd)) {
                 int index = actorList.indexOf(actorToAdd);
                 actorToAdd = actorList.get(index);
                 flixCastIn = actorToAdd.getMovies();
-                if(!flixCastIn.contains(movie)) {
+                if (!flixCastIn.contains(movie)) {
                     flixCastIn.add(movie);
                     actorToAdd.setMovies(flixCastIn);
                 }
@@ -53,7 +54,7 @@ public class MovieDatabase {
         ArrayList<Actor> result = new ArrayList<Actor>();
         for (String actorName : actors) {
             actor = new Actor(actorName);
-            if(actorList.contains(actor)) {
+            if (actorList.contains(actor)) {
                 actor = actorList.get(actorList.indexOf(actor));
                 actor = addMovieToList(actor, movie);
             } else {
@@ -75,40 +76,46 @@ public class MovieDatabase {
     /**
      * Add a rating for this movie. Assume that the name argument will definitely be a name that is currently
      * in the database.
-     * @param name Name of the movie which is going to have it's rating added
+     *
+     * @param name   Name of the movie which is going to have it's rating added
      * @param rating Double value for new rating
      */
     void addRating(String name, double rating) {
         int index = movieList.indexOf(new Movie(name));
-        Movie movie = movieList.get(index);
-        movie.setRating(rating);
-        movieList.set(index, movie);
+        if(index != -1) {
+            Movie movie = movieList.get(index);
+            movie.setRating(rating);
+            movieList.set(index, movie);
+        }
     }
 
     /**
      * Overwrite the current rating of a movie with this new rating. Again, assume that the name argument will
      * definitely be a name that is currently in the database.
-     * @param name Name of the movie which is going to have it's rating updated
+     *
+     * @param name      Name of the movie which is going to have it's rating updated
      * @param newRating Double value for new rating
      */
     void updateRating(String name, double newRating) {
         int index = movieList.indexOf(new Movie(name));
-        Movie movie = movieList.get(index);
-        movie.setRating(newRating);
-        movieList.set(index, movie);
+        if(index != -1) {
+            Movie movie = movieList.get(index);
+            movie.setRating(newRating);
+            movieList.set(index, movie);
+        };
     }
 
     /**
      * @return Returns the name of the actor that has the best average rating for their movies.
      */
     String getBestActor() {
-        if(actorList.isEmpty()) {
+        if (actorList.isEmpty()) {
             return null;
         }
 
         Actor result = actorList.get(0);
         for (Actor actor : actorList) {
-            if(avgRating(actor.getMovies()) > avgRating(result.getMovies())) {
+            if (avgRating(actor.getMovies()) > avgRating(result.getMovies())) {
                 result = actor;
             }
         }
@@ -120,17 +127,18 @@ public class MovieDatabase {
     }
 
     /**
-     *
      * @return Returns the name of the movie with the highest rating.
      */
     String getBestMovie() {
-        if(movieList.isEmpty()) {
+        if (movieList.isEmpty()) {
             return null;
         }
 
         Movie result = movieList.get(0);
-        for (Movie movie: movieList) {
-            if(result.getRating() < movie.getRating()) { result = movie; }
+        for (Movie movie : movieList) {
+            if (result.getRating() < movie.getRating()) {
+                result = movie;
+            }
         }
         return result.getName();
     }
